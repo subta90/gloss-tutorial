c5fa4b96a65b9c880e70a6a74b08b9e4f210f1fe
subta90 <the.bird.nobility298@gmail.com>
2020-02-24 17:47:22 +0900

    play example

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Game
 
 windowWidth, windowHeight :: Num a => a
 windowWidth = 1280
@@ -22,26 +23,52 @@ data BoxState = BoxState
     }
 
 initialBox :: BoxState
-initialBox = BoxState 0 0 150 150
+initialBox = BoxState 0 0 0 0
 
 drawBox :: BoxState -> Picture
 drawBox box = translate (_x box) (_y box) $ rectangleSolid boxWidth boxHeight
 
-nextBox :: ViewPort -> Float -> BoxState -> BoxState
-nextBox vp dt box = 
+updateBox :: Event -> BoxState -> BoxState
+updateBox (EventKey key ks _ _) box = updateBoxWithKey key ks box
+updateBox (EventMotion _)       box = box
+updateBox (EventResize _)       box = box
+
+up, down, right, left :: BoxState -> BoxState
+up box = box { _vy = _vy box + 300 }
+down box = box { _vy = _vy box - 300 }
+right box = box { _vx = _vx box + 300 }
+left box = box { _vx = _vx box - 300 } 
+
+updateBoxWithKey :: Key -> KeyState -> BoxState -> BoxState
+updateBoxWithKey (SpecialKey KeyUp) ks = if ks == Down then up else down
+updateBoxWithKey (SpecialKey KeyDown) ks = if ks == Down then down else up
+updateBoxWithKey (SpecialKey KeyRight) ks = if ks == Down then right else left
+updateBoxWithKey (SpecialKey KeyLeft) ks = if ks == Down then left else right
+updateBoxWithKey (Char 'w') ks = if ks == Down then up else down
+updateBoxWithKey (Char 's') ks = if ks == Down then down else up
+updateBoxWithKey (Char 'd') ks = if ks == Down then right else left
+updateBoxWithKey (Char 'a') ks = if ks == Down then left else right
+updateBoxWithKey _ _ = id 
+
+nextBox :: Float -> BoxState -> BoxState
+nextBox dt box =
     let
         x = _x box + _vx box * dt
         y = _y box + _vy box * dt
 
-        isOverTop = y > (windowHeight - boxHeight) / 2
-        isOverBottom = y < -(windowHeight - boxHeight) / 2
-        isOverRight = x > (windowWidth - boxWidth) / 2
-        isOverLeft = x < -(windowWidth - boxWidth) / 2
+    in box { _x = correctedX x, _y = correctedY y}
 
-        vx = if isOverRight || isOverLeft then (-_vx box) else (_vx box)
-        vy = if isOverTop   || isOverBottom then (-_vy box) else (_vy box)
+correctedX :: Float -> Float
+correctedX x 
+    | x > (windowWidth - boxWidth) / 2 = (windowWidth - boxWidth) / 2
+    | x < -(windowWidth - boxWidth) / 2 = -(windowWidth - boxWidth) / 2
+    | otherwise = x
 
-    in BoxState x y vx vy
+correctedY :: Float -> Float
+correctedY y 
+    | y > (windowHeight - boxHeight) / 2 = (windowHeight - boxHeight) / 2
+    | y < -(windowHeight - boxHeight) / 2 = -(windowHeight - boxHeight) / 2
+    | otherwise = y
 
 main :: IO ()
-main = simulate window white 24 initialBox drawBox nextBox
+main = play window white 24 initialBox drawBox updateBox nextBox
